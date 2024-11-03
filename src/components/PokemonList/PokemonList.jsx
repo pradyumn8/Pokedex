81cb4fd865a78fd7c4f647249a66e3b275034e2c
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-03 17:34:51 +0530

    fetched data from API of next and prev

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -8,14 +8,22 @@ function PokemonList() {
     const [pokemonList, setPokemonList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
-    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon';
+    const [pokedexUrl,setPokedexUrl] =useState('https://pokeapi.co/api/v2/pokemon');
+
+    const [nextUrl, setNextUrl] = useState([])
+    const [prevUrl, setPrevUrl] = useState(true)
 
     async function downloadPokemons() {
-        const response = await axios.get(POKEDEX_URL); // It help to fetch 20 pokemon
+        setIsLoading(true);
+        const response = await axios.get(pokedexUrl); // It help to fetch 20 pokemon
 
         const pokemonResults = response.data.results; // we get the array of pokemons from results
 
-        console.log(pokemonResults)
+        // fetched data from API of next and prev
+        console.log(response.data);
+        setNextUrl(response.data.next);
+        setPrevUrl(response.data.previous);
+
         // iterating over the array of pokemons, and using thier url, to create an array of promises
         // that will download those 20 pokemons
         const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url)); //
@@ -42,21 +50,20 @@ function PokemonList() {
         setIsLoading(false);
     }
 
-    useEffect(async () => {
+    useEffect( () => {
         downloadPokemons()
-    }, []);
+    }, [pokedexUrl]);
 
     return (
         <div className='pokemon-list-wrapper'>
-            <div>Pokemon List</div>
             <div className='pokemon-wrapper'>
                 {(isLoading) ? 'Loading....' :
                     pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} />)
                 }
             </div>
             <div className="controls">
-                <button>Prev</button>
-                <button>Next</button>
+                <button disabled={prevUrl== null} onClick={()=>setPokedexUrl(prevUrl)}>Prev</button>
+                <button disabled={nextUrl== null} onClick={()=>setPokedexUrl(nextUrl)}>Next</button>
             </div>
         </div>
     );

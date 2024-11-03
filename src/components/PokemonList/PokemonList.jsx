136dca387e16bd18aea07410837c5420b7907db2
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-03 16:36:02 +0530

    API fetched Properly on UI

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './PokemonList.css'
+import Pokemon from '../Pokemon/Pokemon';
 
 function PokemonList() {
-    const [x, setX] = useState(0);  // Declare x before useEffect
-    const [y, setY] = useState(0);
 
-    useEffect(() => {
-        console.log('Effect Called');
-    }, [x]);  // Now x is correctly referenced in the dependency array
+    const [pokemonList, setPokemonList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function downloadPokemons(){
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        const pokemonResults = response.data.results;
+        const pokemonResultPromise=pokemonResults.map((pokemon)=>axios.get(pokemon.url))
+        console.log(pokemonResultPromise);
+        const pokemonData = await axios.all(pokemonResultPromise);
+        console.log(pokemonData)
+        setPokemonList(pokemonData.map((pokemonData)=>{
+            const pokemon = pokemonData.data;
+            return { 
+                id: pokemon.id,
+                name:pokemon.name,
+                image:(pokemon.sprites.other)?pokemon.sprites.other.dream_world.front_default:pokemonData.sprites.front_shiny,
+                types:pokemonList.types}
+        }))
+        setIsLoading(false);
+    }
+
+    useEffect(async() => {
+        downloadPokemons()
+    }, []);  
 
     return (
-        <>
-            <div>
-                X: {x} <button onClick={() => setX(x + 1)}>Inc</button>
-            </div>
-
-            <div>
-                Y: {y} <button onClick={() => setY(y + 1)}>Dec</button>
-            </div>
-        </>
+    <div className='pokemon-list-wrapper'>
+        <div>Pokemon List</div>
+        {(isLoading)?'Loading....':
+    pokemonList.map((p)=><Pokemon name={p.name} image={p.image} key={p.id}/>)
+    }
+    </div>
     );
 }
 
